bffdd5ffe060c3dc83e7ad9d24228176674ca143
Sven Petersen <s.petersen@webever.de>
2022-02-21 10:52:45 +0100

    custom start page

--- generate_site.sh
+++ generate_site.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr antora sven-antora-playbook.yml
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr antora --fetch sven-antora-playbook.yml
 touch build/site/.nojekyll
