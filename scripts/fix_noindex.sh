5465225c432f50c7c5b1396b085d1a9f1a7dd491
Felix Hennig <mail@felixhennig.com>
2022-12-13 16:45:04 +0100

    Added description to the script, why we're doing what we're doing

--- scripts/fix_noindex.sh
+++ scripts/fix_noindex.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This script removes the 'noindex' meta tag from the main entrypoint into the docs.
+# The index.html is a 301 redirect to the /home/stable/index.html path, where the landing page is.
+# 
+# We have problems with Google not indexing the docs, and we hope that this might fix these
+# problems. 
+#
+# The github issue: https://github.com/stackabletech/documentation/issues/324
 
 index_file="$(dirname "$0")/../build/site/index.html"
 
