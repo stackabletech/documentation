0fe1fd66b3ef24aabf4978cd17a1719594e88f9a
Felix Hennig <mail@felixhennig.com>
2023-04-24 14:47:52 +0200

    ...

--- scripts/netlify_git_fetch.sh
+++ scripts/netlify_git_fetch.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 
+# netlify messes with some files, restore everything to how it was
+git restore .
+
+# save current commit for later
 current_commit=$(git rev-parse HEAD)
 
+# update
 git fetch --all
 
-git stash
-
+# checkout all release branches once, so we fetch the files
 for remote in $(git branch -r | grep release/); do
     git switch "${remote#origin/}"
 done
 
+# go back to the initial commit to start the build
 git checkout "$current_commit"
 
-git stash pop
