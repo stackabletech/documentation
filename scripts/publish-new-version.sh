8eebf9f6f67b56b7f30e78e7df2537c2be6b82ad
Felix Hennig <mail@felixhennig.com>
2023-12-18 11:53:30 +0100

    Added WIP version of the second release script

--- scripts/publish-new-version.sh
+++ scripts/publish-new-version.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 # This script ... TODO
 
+# Check if yq is installed
+if ! command -v yq &> /dev/null; then
+    echo "Error: 'yq' is not installed. It's needed to update yaml files later in the script."
+    echo "Please install 'yq' from https://github.com/mikefarah/yq and then run the script again."
+    exit 1
+fi
+
 # Check if a version argument is provided
 if [ -z "$1" ]; then
     echo "Please provide a version as a command-line argument (major.minor.patch)."
@@ -61,5 +68,50 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 echo "All checks passed."
+echo "Updating playbooks."
 
-# TODO update playbooks
+# Define the branches to add. The documentation repo uses a '/' while the operators use a '-'
+docs_branch="release/$docs_version"
+operator_branch="release-$docs_version"
+insert_position=1
+
+# List of YAML files to modify
+playbook_files=("antora-playbook.yml" "local-antora-playbook.yml" "gitpod-antora-playbook.yml")
+
+# Loop through each playbook file
+for yaml_file in "${playbook_files[@]}"; do
+    # Insert the docs_branch
+    yq eval ".content.sources[0].branches = .content.sources[0].branches[:$insert_position] + [\"$docs_branch\"] + .content.sources[0].branches[$insert_position:]" -i "$yaml_file"
+
+    # Update all the operator sources
+    yq eval ".content.sources[2:] |= map(.branches = .branches[:$insert_position] + [\"$operator_branch\"] + .branches[$insert_position:])" -i "$yaml_file"
+done
+
+# Display changes using git diff
+git diff
+
+# Ask the user whether to proceed
+read -p "Do you want to proceed with these changes? (yes/no): " proceed_answer
+
+# Convert the user input to lowercase for case-insensitive comparison
+proceed_answer_lowercase=$(echo "$proceed_answer" | tr '[:upper:]' '[:lower:]')
+
+# Check the user's response
+if [ "$proceed_answer_lowercase" != "yes" ]; then
+    echo "Aborted. Nothing was commited."
+    exit 1
+fi
+
+publish_branch="publish-$docs_version"
+
+git checkout -b "$publish_branch"
+
+git add .
+git commit -m "Add release branches to the playbooks for release $docs_version."
+
+# Push the branch upstream
+git push -u origin "$publish_branch"
+
+echo "The changes have been pushed to GitHub!"
+echo "Click the link above to create the PR in GitHub, and then verify that the build works with Netlify previews."
+echo "Once the branch is merged, the changes are live."
