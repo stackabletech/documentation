4d8d168d5f0baba7735a857a454d7d70e33ebab1
Techassi <git@techassi.dev>
2025-07-24 20:43:23 +0200

    refactor: Improve branch/release scripts

--- scripts/publish-new-version.sh
+++ scripts/publish-new-version.sh
@@ -24,20 +24,20 @@ fi
 # Args parsing
 # ------------------------------
 
-docs_version=""
-push=false
+DOCS_VERSION=""
+PUSH=false
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -v|--version) docs_version="$2"; shift ;;
-        -p|--push) push=true ;;
+        -v|--version) DOCS_VERSION="$2"; shift ;;
+        -p|--push) PUSH=true ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
 # Check if the required version argument is provided
-if [ -z "$docs_version" ]; then
+if [ -z "$DOCS_VERSION" ]; then
 echo "Usage: publish-new-version.sh -v <version> [-p]"
 echo "The version needs to be provided as <major>.<minor>."
 echo "Use -p to automatically push at the end."
@@ -45,42 +45,40 @@ exit 1
 fi
 
 # Validate the version format (major.minor.patch)
-if [[ ! "$docs_version" =~ ^[0-9]+\.[0-9]+$ ]]; then
+if [[ ! "$DOCS_VERSION" =~ ^[0-9]+\.[0-9]+$ ]]; then
     echo "Invalid version format. Please use the major.minor format."
     exit 1
 fi
 
-# Define the branches to add. The documentation repo uses a '/' while the operators use a '-'
-docs_branch="release/$docs_version"
-operator_branch="release-$docs_version"
+BRANCH="release-$DOCS_VERSION"
 
 # ------------------------------
 # Checking prerequisites
 # ------------------------------
 
 # Check if the release branch exists upstream
-if ! git rev-parse --quiet --verify "$docs_branch" > /dev/null; then
-    echo "Release branch '$docs_branch' is missing upstream in the documentation repository."
-    echo "Please create the $docs_branch branch first using the make-release-branch.sh script."
+if ! git rev-parse --quiet --verify "$BRANCH" > /dev/null; then
+    echo "Release branch '$BRANCH' is missing upstream in the documentation repository."
+    echo "Please create the $BRANCH branch first using the make-release-branch.sh script."
     echo "Aborting."
     exit 1
 fi
 
 echo "Did you create all the release branches in the operators?"
 echo "Did you also create a release branch in the demos repository?"
-read -p "(yes/no): " operators_branches_answer
+read -r -p "(yes/no): " OPERATORS_BRANCHES_ANSWER
 
 # Convert the user input to lowercase for case-insensitive comparison
-operators_branches_answer_lowercase=$(echo "$operators_branches_answer" | tr '[:upper:]' '[:lower:]')
+OPERATORS_BRANCHES_ANSWER_LOWERCASE=$(echo "$OPERATORS_BRANCHES_ANSWER" | tr '[:upper:]' '[:lower:]')
 
-if [ "$operators_branches_answer_lowercase" != "yes" ]; then
+if [ "$OPERATORS_BRANCHES_ANSWER_LOWERCASE" != "yes" ]; then
     echo "Please create all the branches in the operators before proceeding."
     exit 1
 fi
 
 # Check if on main branch
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$current_branch" != "main" ]; then
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != "main" ]; then
     echo "Not on the main branch. Please switch to the main branch."
     exit 1
 fi
@@ -106,21 +104,13 @@ echo "All checks passed."
 
 echo "Updating playbooks."
 
-# Define the branches to add. The documentation repo uses a '/' while the operators use a '-'
-docs_branch="release/$docs_version"
-operator_branch="release-$docs_version"
-insert_position=1
-
-docs_dir="$(dirname "$0")/.."
-playbook_files=("$docs_dir/antora-playbook.yml" "$docs_dir/local-antora-playbook.yml")
+DOCS_DIRECTORY="$(dirname "$0")/.."
+PLAYBOOK_FILES=("$DOCS_DIRECTORY/antora-playbook.yml" "$DOCS_DIRECTORY/local-antora-playbook.yml")
 
 # Loop through each playbook file
-for yaml_file in "${playbook_files[@]}"; do
-    # Insert the docs_branch
-    yq ".content.sources[0].branches |= (.[:$insert_position] + [\"$docs_branch\"] + .[$insert_position:])" -i "$yaml_file"
-
-    # Update all the operator and demos sources.
-    yq "with(.content.sources.[]; select(.url |test(\".*(operator|demos).*\")) | .branches |= .[:$insert_position] + [\"$operator_branch\"] + .[$insert_position:])" -i "$yaml_file"
+for yaml_file in "${PLAYBOOK_FILES[@]}"; do
+    # Update all sources except stackable-cockpit.
+    yq "with(.content.sources.[]; select(.url | test(\".*(stackable-cockpit).*\") | not) | .branches |= .[:1] + [\"release-25.7\"] + .[1:])" -i "$yaml_file"
 done
 
 # ------------------------------
@@ -129,35 +119,37 @@ done
 
 # Display changes and ask for user confirmation
 git diff
-read -p "Do you want to proceed with these changes? (yes/no): " proceed_answer
+read -r -p "Do you want to proceed with these changes? (yes/no): " PROCEED_ANSWER
 
 # Convert the user input to lowercase for case-insensitive comparison
-proceed_answer_lowercase=$(echo "$proceed_answer" | tr '[:upper:]' '[:lower:]')
+PROCEED_ANSWER_LOWERCASE=$(echo "$PROCEED_ANSWER" | tr '[:upper:]' '[:lower:]')
 
 # Check the user's response
-if [ "$proceed_answer_lowercase" != "yes" ]; then
-    echo "Aborted. Nothing was commited."
+if [ "$PROCEED_ANSWER_LOWERCASE" != "yes" ]; then
+    echo "Aborted. Nothing was committed."
     exit 1
 fi
 
-publish_branch="publish-$docs_version"
+PR_BRANCH="pr-$DOCS_VERSION"
 
-git checkout -b "$publish_branch"
+git checkout -b "$PR_BRANCH"
 
 git add .
-git commit -m "Add release branches to the playbooks for release $docs_version"
+git commit -m "chore: Add release branches to the playbooks for release $DOCS_VERSION"
 
 # Push the branch if requested
-if [ "$push" = true ]; then
+if [ "$PUSH" = true ]; then
     echo "Pushing changes to origin ..."
-    git push -u origin "$publish_branch"
+    git push -u origin "$PR_BRANCH"
     echo ""
     echo "The changes have been pushed to GitHub!"
+    echo "Raise the PR against the $BRANCH branch."
     echo "Click the link above to create the PR in GitHub, and then verify that the build works with Netlify previews."
     echo "Once the branch is merged, the changes will automatically be deployed and be live."
 else
     echo ""
     echo "Skipping push to origin."
-    echo "Please push the branch manually and create  PR."
+    echo "Please push the branch manually and create PR."
+    echo "Raise the PR against the $BRANCH branch."
     echo "Once the changes are merged, they will automatically be deployed and be live."
 fi
